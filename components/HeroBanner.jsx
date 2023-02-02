98fae40bba32761ed9d5d74f38b41897e6a3b1eb
Farhaan Noor <farhanoor@outlook.com>
2023-02-02 22:56:59 +0530

    second commit

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -1,16 +1,20 @@
-import React from 'react';
-import Link from 'next/link';
+import React from "react";
+import Link from "next/link";
 
-import { urlFor } from 'lib/client';
+import {urlFor} from "lib/client";
 
-const HeroBanner = ({ heroBanner }) => {
+const HeroBanner = ({heroBanner}) => {
   return (
-    <div className='hero-banner-container'>
+    <div className="hero-banner-container">
       <div>
         <p className="beats-solo">{heroBanner.smallText}</p>
-        <h3>{heroBanner.midText}</h3>
-        <h1>{heroBanner.largeText1}</h1>
-        <img src={urlFor(heroBanner.image)} alt="headphones" className='hero-banner-image' />
+        <h3 className="white">{heroBanner.midText}</h3>
+        <h1 className="white">{heroBanner.largeText1}</h1>
+        <img
+          src={urlFor(heroBanner.image)}
+          alt="headphones"
+          className="hero-banner-image"
+        />
         <div>
           <Link href={`/product/${heroBanner.product}`}>
             <button type="button">{heroBanner.buttonText}</button>
@@ -21,9 +25,8 @@ const HeroBanner = ({ heroBanner }) => {
           </div>
         </div>
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default HeroBanner
+export default HeroBanner;
